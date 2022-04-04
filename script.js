b370952bce866311bff7ed46d8813cfe0772e331
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2022-04-04 07:51:33 -0400

    Adding text and working in javascript

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "";
 
 document.querySelector("body").appendChild(h2);
+
+const input = document.getElementById('sidenav');
+input.addEventListener('click', function() {
+let red = (Math.random() * 100) + 150;
+let blue = (Math.random() * 100) + 150;
+let green = (Math.random() * 100) + 150;
+let backgroundColor = `rgb (${red}, ${blue}, ${green}`;
+document.body.style.background = backgroundColor;
+});
